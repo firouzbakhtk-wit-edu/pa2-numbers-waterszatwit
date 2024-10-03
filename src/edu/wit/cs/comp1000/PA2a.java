a65947d5360f8e92414cb4b1c2feb2ef4719819d
Zackary Waters <watersz@wit.edu>
2024-10-03 16:10:39 -0400

    PA2a and b

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,71 @@
 package edu.wit.cs.comp1000;
+import java.util.Scanner;
 
-// TODO: document this class
+/**
+ * author @ZackaryWaters
+ */
 public class PA2a {
-
-	// TODO: document this function
+/**
+ * starts and runs programs default constructor
+ * @param args
+ */
 	public static void main(String[] args) {
-		// TODO: write your code here
-	}
 
+		Scanner input = new Scanner(System.in);
+
+		/**
+		 * Variable declaration including array
+		 */
+		int[] numbers = new int[5]; 
+		int sumPos = 0, sumNeg = 0, sumAll = 0;
+		int countPos = 0, countNeg = 0;
+		
+		System.out.printf("Enter five whole numbers: ");
+		for (int i = 0; i < 5; i++) { //for loop iterates through numbers provided and adds to array
+			numbers[i] = input.nextInt();
+			sumAll += numbers[i]; // adds to the sumAll variable
+			if (numbers[i] > 0) { // determines if number is positive and add to sumPos and increments countPos
+				sumPos += numbers[i];
+				countPos++;
+			} else if (numbers[i] <= 0) { // determines if number is negative and adds to sumNeg and increments countNeg
+				sumNeg += numbers[i];
+				countNeg++;
+			}
+		}
+		
+		double avgPos = countPos > 0 ? (double) sumPos/countPos : 0; // determines if countPos is greater than 0 if it is then casts double to the avg calulation
+		double avgNeg = countNeg > 0 ? (double) sumNeg/countNeg : 0; // determines if countNeg is greater than 0 if it is then casts double to the avg calulation
+		double avgAll = (double) sumAll/5; // casts double for avgAll
+		
+		if(countPos==1) {
+			System.out.printf("The sum of the %d positive number: %d%n", countPos, sumPos);
+
+		} else {
+			System.out.printf("The sum of the %d positive numbers: %d%n", countPos, sumPos);
+
+		}
+		if(countNeg == 1) {
+			System.out.printf("The sum of the %d non-positive number: %d%n", countNeg, sumNeg);
+		} else {
+			System.out.printf("The sum of the %d non-positive numbers: %d%n", countNeg, sumNeg);
+		}
+		
+		System.out.printf("The sum of the 5 numbers: %d%n", sumAll);
+		
+		if(countPos==1) {
+			System.out.printf("The average of the %d positive number: %.2f%n", countPos, avgPos);
+		} else {
+			System.out.printf("The average of the %d positive numbers: %.2f%n", countPos, avgPos);
+		}
+		
+		if(countNeg == 1) {
+			System.out.printf("The average of the %d non-positive number: %.2f%n", countNeg, avgNeg);
+		} else {
+			System.out.printf("The average of the %d non-positive numbers: %.2f%n", countNeg, avgNeg);
+		}
+		
+		System.out.printf("The average of the 5 numbers: %.2f%n", avgAll);
+		
+		input.close();
+	}
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,45 @@
 package edu.wit.cs.comp1000;
+import java.util.Scanner;
 
-// TODO: document this class
+/**
+ * author @ZackaryWaters
+ */
 public class PA2b {
-
-	// TODO: document this function
+	/**
+	 * main method starts and runs program; default constructor
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		
+		Scanner input = new Scanner(System.in);
+		
+		double a;
+		double b;
+		double c;
+		
+		System.out.printf("Enter a b c: ");
+		a = input.nextDouble();
+		b = input.nextDouble();
+		c = input.nextDouble();
+		
+		double d = b * b - 4 * a * c; // calculates discriminant 
+		
+		if (d < 0) {
+			System.out.printf("Roots: imaginary%n"); 
+		} else if (d == 0) { // provides the single root of there is exactly one
+			double root = -b / (2 * a);
+			System.out.printf("Root: %.2f%n", root);
+		} else { // determines the two roots of the discriminant 
+			double root1 = (-b + Math.sqrt(d)) / (2 * a);
+			double root2 = (-b - Math.sqrt(d)) / (2 * a);
+			
+			if (root1 > root2) { // reorders the roots if need be to make sure the least valuable one is printed first
+				double temp = root1;
+				root1 = root2;
+				root2 = temp;
+			}
+			System.out.printf("Roots: %.2f, %.2f%n", root1, root2);
+		}
+		input.close();
 	}
-
 }
